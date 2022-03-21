e5911c54835700331b4668e801ee0bffcc902240
Karanja-JK <karanjajesse06@gmail.com>
2022-03-21 16:00:14 -0700

    task9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+# include <stdio>
+
+/**
+ * _strcpy - copies strings pointed to by
+ * src including the terminating null byte
+ * to the buffer pointed to by dest
+ * @src: cadena a copiar
+ * @dest: destino de la cadena
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
