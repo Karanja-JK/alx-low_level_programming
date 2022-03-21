1e922faf36128e81f7a697d1c198485341c93424
Karanja-JK <karanjajesse06@gmail.com>
2022-03-21 14:59:42 -0700

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,15 @@
 # include <stdio.h>
 
 /**
- * task 4
+ * print_rev - prints string in reverse
+ * followed by a new line
+ * @str: in reverse
  */
 void print_rev(char *str)
 {
 	int i = 0;
 	int total_characters = 0;
-	int c =0;
+	int c = 0;
 
 	while (str[i] != 0)
 	{
