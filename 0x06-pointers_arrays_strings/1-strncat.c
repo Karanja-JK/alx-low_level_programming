0ec47a54bfe003115cfb6e9edce3c2a7f027b134
Karanja-JK <karanjajesse06@gmail.com>
2022-03-24 13:25:59 -0700

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+# include <stdio.h>
+
+/**
+ * _strcat-concatenates two strings
+ * @dest-pointer to a character will be changed
+ * @src-pointer to a character that will also be changed
+ * Return-dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
